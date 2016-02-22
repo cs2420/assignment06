3f446a07082c9f5322370d55767f2811dd0dc765
douggarding <doug.garding@outlook.com>
2016-02-22 13:57:55 -0700

    Finished implementing the rest of the messages, except for the iterator.

--- assignment06/src/assignment06/DoublyLinkedList.java
+++ assignment06/src/assignment06/DoublyLinkedList.java
@@ -3,6 +3,11 @@ package assignment06;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * An implementation of a Linked List.
+ * 
+ * @author Connor and Doug Garding
+ */
 public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 
 	// number of elements in the list
@@ -12,25 +17,28 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 	// last node
 	private Node<E> last;
 
-	// constructor
+	/**
+	 * Default constructor for a doubly linked list.
+	 */
 	public DoublyLinkedList() {
 		size = 0;
-
 	}
 
-	// Node class
+	/**
+	 * Node Class that stores elements for a linked list.
+	 *
+	 * @param <E>
+	 */
 	private static final class Node<E> {
-		/** The element in the list. */
+		// The element in the list.
 		E data;
-
-		/** The next list entry, null if this is last. */
+		// The next list entry, null if this is last.
 		Node<E> next;
-
-		/** The previous list entry, null if this is first. */
+		// The previous list entry, null if this is first.
 		Node<E> previous;
 
 		/**
-		 * Construct an entry.
+		 * Default constructor for a node.
 		 * 
 		 * @param data
 		 *            the list element
@@ -38,7 +46,6 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 		Node(E data) {
 			this.data = data;
 		}
-
 	}
 
 	@Override
@@ -47,6 +54,9 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 		return null;
 	}
 
+	/**
+	 * Inserts the specified element at the beginning of the list.
+	 */
 	@Override
 	public void addFirst(E element) {
 		Node<E> n = new Node<E>(element);
@@ -57,12 +67,14 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 			n.next = first;
 			first.previous = n;
 			first = n;
-
 		}
 		size++;
 
 	}
 
+	/**
+	 * Inserts the specified element at the end of the list.
+	 */
 	@Override
 	public void addLast(E o) {
 		Node<E> n = new Node<E>(o);
@@ -73,12 +85,16 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 			n.previous = last;
 			last.next = n;
 			last = n;
-
 		}
 		size++;
 
 	}
 
+	/**
+	 * Inserts the specified element at the specified position in the list.
+	 * Throws IndexOutOfBoundsException if index is out of range (index < 0 ||
+	 * index > size())
+	 */
 	@Override
 	public void add(int index, E element) throws IndexOutOfBoundsException {
 		if (index > size || index < 0) {
@@ -106,6 +122,10 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 
 	}
 
+	/**
+	 * Returns the first element in the list. Throws NoSuchElementException if
+	 * the list is empty.
+	 */
 	@Override
 	public E getFirst() throws NoSuchElementException {
 		if (size == 0) {
@@ -114,6 +134,10 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 		return first.data;
 	}
 
+	/**
+	 * Returns the last element in the list. Throws NoSuchElementException if
+	 * the list is empty.
+	 */
 	@Override
 	public E getLast() throws NoSuchElementException {
 		if (size == 0) {
@@ -122,6 +146,11 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 		return last.data;
 	}
 
+	/**
+	 * Returns the element at the specified position in the list. Throws
+	 * IndexOutOfBoundsException if index is out of range (index < 0 || index >=
+	 * size())
+	 */
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
 		if (index < 0 || index > size - 1) {
@@ -130,6 +159,10 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 		return getNode(index).data;
 	}
 
+	/**
+	 * Removes and returns the first element from the list. Throws
+	 * NoSuchElementException if the list is empty.
+	 */
 	@Override
 	public E removeFirst() throws NoSuchElementException {
 		if (size == 0) {
@@ -147,6 +180,10 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 		return result;
 	}
 
+	/**
+	 * Removes and returns the last element from the list. Throws
+	 * NoSuchElementException if the list is empty.
+	 */
 	@Override
 	public E removeLast() throws NoSuchElementException {
 		if (size == 0) {
@@ -164,20 +201,24 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 		return result;
 	}
 
+	/**
+	 * Removes and returns the element at the specified position in the list.
+	 * Throws IndexOutOfBoundsException if index is out of range (index < 0 ||
+	 * index >= size())
+	 */
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
-		if (index > size-1 || index < 0) {
+		if (index > size - 1 || index < 0) {
 			throw new IndexOutOfBoundsException();
 		}
+
 		Node<E> n = getNode(index);
 		E result = n.data;
-		if(size == 1 || index == 0){
+		if (size == 1 || index == 0) {
 			removeFirst();
-		}
-		else if(index == size-1){
+		} else if (index == size - 1) {
 			removeLast();
-		}
-		else{
+		} else {
 			n.previous.next = n.next;
 			n.next.previous = n.previous;
 			size--;
@@ -185,40 +226,100 @@ public class DoublyLinkedList<E> implements List<E>, Iterable<E> {
 		return result;
 	}
 
+	/**
+	 * Returns the index of the first occurrence of the specified element in the
+	 * list, or -1 if this list does not contain the element.
+	 */
 	@Override
 	public int indexOf(E element) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		// If the DoublyLinkedList is empty
+		if (this.size == 0)
+			return -1;
+
+		// Check the element of each node, starting at beginning
+		Node<E> currentNode = this.first;
+		for (int i = 0; i < this.size; i++){
+			if (currentNode.data == element)
+				return i;
+			currentNode = currentNode.next;
+		}
+
+		// If element was not found
+		return -1;
 	}
 
+	/**
+	 * Returns the index of the last occurrence of the specified element in this
+	 * list, or -1 if this list does not contain the element.
+	 */
 	public int lastIndexOf(E element) {
-		// TODO Auto-generated method stub
-		return 0;
+		// If the DoublyLinkedList is empty
+		if (this.size == 0)
+			return -1;
+
+		// Check the element of each node, starting at end
+		Node<E> currentNode = this.last;
+		for (int i = this.size - 1; i >= 0; i--){
+			if (currentNode.data == element)
+				return i;
+			currentNode = currentNode.previous;
+		}
+	
+		// If element was not found
+		return -1;
 	}
 
+	/**
+	 * Returns the number of elements in this list.
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
 
+	/**
+	 * Returns true if this collection contains no elements.
+	 */
 	@Override
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
+	/**
+	 * Removes all of the elements from this list.
+	 */
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		this.first.next = null;
+		this.first.data = null;
+		this.last.previous = null;
+		this.last.data = null;
+		size = 0;
 	}
 
+	/**
+	 * Returns an array containing all of the elements in this list in proper
+	 * sequence (from first to last element).
+	 */
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+
+		Object elementArray[] = new Object[this.size];
+		Node<E> currentNode = this.first;
+		
+		// Add each element in DoublyLinkedList to array
+		for (int i = 0; i < this.size; i++){
+			elementArray[i] = currentNode.data;
+			currentNode = currentNode.next;
+		}
+
+		return elementArray;
 	}
 
-	// Helper method, returns the node at index of i
+	/**
+	 * Helper method, returns the node at index of i
+	 */
 	private Node<E> getNode(int index) {
 		Node<E> n;
 		int i = 0;
